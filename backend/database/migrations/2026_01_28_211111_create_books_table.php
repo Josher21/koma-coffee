bdde650a1a80a16c367ed8673e661ff3cc7c7df1
Josher21 <sanchezhernandez2198@gmail.com>
2026-01-28 23:54:06 +0100

    book attributes fixed

--- backend/database/migrations/2026_01_28_211111_create_books_table.php
+++ backend/database/migrations/2026_01_28_211111_create_books_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->string('author');
+            $table->string('editorial');
             $table->unsignedSmallInteger('pages')->nullable();
             $table->text('synopsis')->nullable();
             $table->string('image')->nullable();    // Por ahora URL
